69d5b1393c46439de8fb534cbece82cfe165874f
USERSAD\rpaezbas <rpaezbas@everis.com>
2018-04-11 14:21:35 +0200

    Authorization for single get working

--- src/main/java/resources/Rest.java
+++ src/main/java/resources/Rest.java
@@ -4,6 +4,7 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -15,7 +16,7 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import entities.Car;
-
+import Auth.AuthUtil;
 
 @Path("/")
 public class Rest {
@@ -46,19 +47,30 @@ public class Rest {
 	@GET
 	@Path("/{id}")
 	@Produces("application/json")
-	public Response getCarbyId(@PathParam("id") int id) {
+	public Response getCarbyId(@PathParam("id") int id, @HeaderParam("authorization") String authorization) {
 		Response response;
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 		Transaction transaction = session.beginTransaction();
 		try {
-			Car car = (Car) session.get(Car.class, id);
-			session.getTransaction().commit();
-			if (car != null) {
-				response = Response.status(201).entity(car).build();
+			//Check if the token in the header can be verified
+			if (AuthUtil.verifyTokenInHeader(authorization)) {
+				
+				Car car = (Car) session.get(Car.class, id);
+				session.getTransaction().commit();
+				
+				if (car != null) {
+					response = Response.status(201).entity(car).build();
+				} else {
+					response = Response.status(404).build();
+				}
+				
+			//If the token doesnt verify
 			} else {
-				response = Response.status(404).build();
+				response = Response.status(401).build();
 			}
+			
 		} catch (HibernateException hibernateEx) {
+			
 			try {
 				transaction.rollback();
 				response = Response.status(500).build();
@@ -69,7 +81,9 @@ public class Rest {
 				rollbackEx.printStackTrace();
 				session.close();
 			}
+			
 		}
+
 		return response;
 	}
 
