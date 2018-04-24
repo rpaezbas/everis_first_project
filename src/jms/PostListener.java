d7c65e61542d2cb7080f526071e1bd3b5263cd3a
USERSAD\rpaezbas <rpaezbas@everis.com>
2018-04-24 14:06:53 +0200

    Implemented postListener

--- src/jms/PostListener.java
+++ src/jms/PostListener.java
@@ -1,27 +1,28 @@
 package jms;
 
-import javax.ejb.EJB;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import cars.boundary.Controller;
 import cars.entity.Car;
 
 class PostListener implements MessageListener {
 
-	static Gson gson = new Gson();
-
-	@EJB
-	Controller statelessCrudEJB;
+	//This constructor specifies the dateFormat expected in the json object
+	Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss").create();
+	Controller crudController = new Controller();
 
 	@Override
 	public void onMessage(Message message) {
 		TextMessage textMessage = (TextMessage) message;
 		try {
-			System.out.println("Type: " +  textMessage.getJMSType());
-			System.out.println("PostListener received message: " + textMessage.getText());
+			String jsonCar = textMessage.getText();
+			Car carToSave = gson.fromJson(jsonCar,Car.class);
+			crudController.postCar(carToSave);
 		} catch (JMSException e) {
 			e.printStackTrace();
 		}
